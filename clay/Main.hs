rekahsoft/blog-rekahsoft-ca

commit b0851fc6fa9e1411991314871a50559ec3e69149

Fixed li bullet indicator
Use the unicode arrow '2192' infront of ol > li bullets. This must have
been missed while porting css from sass to clay.

Signed-off-by: Collin J. Doering <collin.doering@rekahsoft.ca>

clay/Main.hs
@@ -137,7 +137,7 @@ pageContent = do
   "#page-content" # ".loading" ?
     opacity 0.35
 
-  "#page-content" # ".lading-done" ? do
+  "#page-content" # ".loading-done" ? do
     transition "opacity" (sec 1) easeIn (sec 0.5)
     opacity 1
 
@@ -156,6 +156,11 @@ pageContent = do
     backgroundRepeat noRepeat
     backgroundPosition $ positioned (pct 50) (pct 50)
     height (px 125)
+
+  "#page-content" ** ul |> (li # ":before") ? do
+    content $ stringContent "\\2192"
+    paddingRight (em 0.5)
+    fontSize (em 1.5)
 
 theFooter :: Css
 theFooter = do

commit 110b6a0e6469d3527a3ef40a7e407febee4dfb75

Fixed issue with ol > li's not being numbered
Issue introduced in b0851fc where in ordered lists, li's were
unnumbered.

Signed-off-by: Collin J. Doering <collin.doering@rekahsoft.ca>

clay/Main.hs
@@ -131,9 +131,11 @@ pageContent = do
     marginTop (em 1)
     overflow hidden
 
+    ul |> li ? do
+      listStyleType none
+
     (ul <> ol) |> li ? do
       marginBottom (em 0.02)
-      listStyleType none
 
   "#page-content" # ".loading" ?
     opacity 0.35
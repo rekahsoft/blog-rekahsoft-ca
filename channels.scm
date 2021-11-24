f691f99210155d2b61122066c5f9248cf02a395b
Collin J. Doering <collin@rekahsoft.ca>
2021-11-23 23:06:35 -0500

    WIP: Use newly added channel.scm guix channel file for ci

---
+++ channels.scm
@@ -0,0 +1,20 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "9d370029c21487c54b7bda4aa53e49251107a71f")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'rekahsoft-guix)
+        (url "https://git.rekahsoft.ca/rekahsoft/rekahsoft-guix.git")
+        (commit
+          "191cdaa0947657e0c85fe89ebbb8e7b1e7a8e0a4")
+        (introduction
+          (make-channel-introduction
+            "191cdaa0947657e0c85fe89ebbb8e7b1e7a8e0a4"
+            (openpgp-fingerprint
+              "F8D5 46F3 AF37 EF53 D1B6  48BE 7B4D EB93 212B 3022")))))

4c50f4922bc94b1b81c18dbbbd0530c37fd09cb8
Collin J. Doering <collin@rekahsoft.ca>
2025-08-10 22:02:07 -0400

    channels.scm: Update guix and rekahsoft-guix channels

--- channels.scm
+++ channels.scm
@@ -2,7 +2,7 @@
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (commit
-          "a4e9842a70775a54bbe1369881b739e7ea9a6432")
+          "348c75949043a41dc8771a26f63fd29018aabfea")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -12,7 +12,7 @@
         (name 'rekahsoft-guix)
         (url "https://git.rekahsoft.ca/rekahsoft/rekahsoft-guix.git")
         (commit
-          "e016a7e7a9eb3d27a4d6368861222e9916e27a47")
+          "c7d6d6805034738ff8a4c28891bfa73d33b87043")
         (introduction
           (make-channel-introduction
             "191cdaa0947657e0c85fe89ebbb8e7b1e7a8e0a4"
